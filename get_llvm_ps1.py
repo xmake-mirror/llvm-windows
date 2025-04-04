173b780f0df7f7e5dc9df2f180d9cc23beec5a6b
Neroll <1343809614way@gmail.com>
2025-04-04 13:46:39 +0800

    update powershell

--- get_llvm_ps1.py
+++ get_llvm_ps1.py
@@ -15,7 +15,7 @@ WIN64_PS1_SOURCE = """
 Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-{llvmver}/clang+llvm-{llvmver}-x86_64-pc-windows-msvc.tar.xz" -OutFile llvm.tar.xz
 
 if (-not (Get-Command Expand-7Zip -ErrorAction Ignore)) {{
-    Install-Package -Force -Scope CurrentUser -ProviderName NuGet 7Zip4Powershell > $null
+    Install-Module -Force -Scope CurrentUser -Name 7Zip4Powershell -Confirm:$false > $null
 }}
 
 Expand-7Zip -ArchiveFileName llvm.tar.xz -TargetPath .\\
